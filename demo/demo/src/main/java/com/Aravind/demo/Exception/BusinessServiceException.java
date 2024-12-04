c96a3599e93af4011c201aee3b326968e09a4aa6
Aravind0145 <betharasi.aravind.c@revature.com>
2024-12-04 20:22:22 +0530

    Revert "Add files via upload"
    This reverts commit 9658fab8111fc9098fff91da940ef3ea811f908a.

--- demo/demo/src/main/java/com/Aravind/demo/Exception/BusinessServiceException.java
+++
@@ -1,8 +0,0 @@
-package com.Aravind.demo.Exception;
-
-public class BusinessServiceException extends RuntimeException {
-    public BusinessServiceException(String message, Throwable cause) {
-        super(message, cause);
-
-        }
-    }
--- demo/demo/src/main/java/com/Aravind/demo/Exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.Aravind.demo.Exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
